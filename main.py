859fef34aa15bb8cc9cd41b0968c47a0ca908197
dudcusdltl <75965341+dudcusdltl@users.noreply.github.com>
2025-03-16 11:27:50 +0900

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-print('Hello Mars')
-
-file_path = r"C:\Users\min99\Downloads\mission_computer_main.log"
-
-with open(file_path, 'r', encoding='utf-8') as file:
-    for line in file:
-        print(line.strip())
